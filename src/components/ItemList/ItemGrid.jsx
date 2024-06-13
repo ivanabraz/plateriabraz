63d5593f13693f46c02dd58d21075875fed7c6ed
Ivana Braz <braz.ivana@gmail.com>
2024-06-13 10:52:19 -0300

    Adding product images

--- src/components/ItemList/ItemGrid.jsx
+++ src/components/ItemList/ItemGrid.jsx
@@ -7,15 +7,15 @@ const ItemGrid = ({ productos }) => {
         <div className="mt-6 grid grid-cols-2 gap-x-4 gap-y-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-x-4">
             {productos.map((product) => (
                 <Link to={`/productos/${product.category.toLowerCase()}/${product.id}`} key={uuidv4()} className="group relative">
-                    <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                    <div className="w-full aspect-w-1 aspect-h-1 overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                         <img
                                 src={`${process.env.PUBLIC_URL}/images/products/${product.category.toLowerCase()}/${product.id}.png`}
                                 alt={product.name}
-                            className="object-cover lg:w-full lg:h-full w-full h-full"
+                            className="object-cover w-full h-full"
                         />
                     </div>
                     <div className="mt-4 flex justify-between">
-                        <div>
+                        <div className="pr-3">
                             <h3 className="text-sm font-medium text-neutral-900">
                                 {product.name}
                             </h3>
